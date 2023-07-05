5589a72cbfe18caaa9a33541f5adb912071a8f59
June <sungj1207@gmail.com>
2023-07-05 15:59:57 +0900

    Vue Router 구성

--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@
 // Vue 객체가 없다
 import { createApp } from 'vue'
 import App from './App.vue'
+// Router
+import router from './routes/index.js'
 
 // HTML에서 app이라는 값을 가지고 있는 해당 요소에 vue 프로젝트 연결
 // 1. CDN
 //Vue.createApp(App).mount('#app')
 
 // 2. CLI
-createApp(App).mount('#app')
+createApp(App)
+  .use(router) // plugin으로 연결
+  .mount('#app')
