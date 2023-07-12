1680ac6037a8000a745d912938789da53ead4e19
June <sungj1207@gmail.com>
2023-07-12 14:11:19 +0900

    영화 상세 페이지 정리

--- src/routes/Movie.vue
+++ src/routes/Movie.vue
@@ -1,6 +1,11 @@
 <template>
+  <!-- 
+    v-if = loading 전 skeletons(true)
+    v-else = 영화정보 출력(false)
+   -->
   <div class="container">
-    <div class="skeletons">
+    <template v-if="loading">
+      <div class="skeletons">
       <div class="skeleton poster"></div>
       <div class="specs">
         <div class="skeleton title"></div>
@@ -15,6 +20,46 @@
     :size="3"
     :zIndex="9"
     fixed />
+    </template>
+    <div 
+    v-else  
+    class="movie-details">
+      <div 
+        :style="{ backgroundImage: `url(${theMovie.Poster})` }"
+        class="poster"></div>
+      <div class="specs">
+        <div class="title">
+          {{ theMovie.Title }}
+        </div>
+        <div class="labels">
+          <span>{{ theMovie.Released }}</span>
+          <span>{{ theMovie.Runtime }}</span>
+          <span>{{ theMovie.Country }}</span>
+        </div>
+        <div class="plot">
+          {{ theMovie.Plot }}
+        </div>
+        <div class="ratings">
+          <h3>Ratings</h3>
+        </div>
+        <div>
+          <h3>Actors</h3>
+          {{ theMovie.Actors }}
+        </div>
+        <div>
+          <h3>Director</h3>
+          {{ theMovie.Director }}
+        </div>
+        <div>
+          <h3>Production</h3>
+          {{ theMovie.Production }}
+        </div>
+        <div>
+          <h3>Genre</h3>
+          {{ theMovie.Genre }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -25,6 +70,16 @@ export default {
   components: {
     Loader
   },
+  // 영화 상세 정보
+  computed: {
+    theMovie() {
+      return this.$store.state.movie.theMovie
+    },
+    loading() {
+      return this.$store.state.movie.loading
+    }
+  },
+  // movie.js => _fetchMovie -> theMovie
   created() {
     this.$store.dispatch('movie/searchMovieWithId', {
       // movie/tt12312
@@ -71,6 +126,54 @@ export default {
       width: 50%;
       height: 50px;
       margin-top: 20px;
+    }
+  }
+}
+.movie-details {
+  display: flex; //poster specs 수평
+  color: $gray-600; // 모든 글자 색상 (main.scss)
+  .poster {
+    flex-shrink: 0;
+    width: 500px;
+    height: 500px * 3 / 2;
+    margin-right: 70px;
+    border-radius: 10px;
+    background-color: $gray-200;
+    background-size: cover;
+    background-position: center;
+  }
+  .specs {
+    flex-grow: 1;
+    .title {
+      color: $black;
+      font-family: 'Oswald', sans-serif;
+      font-size: 70px;
+      line-height: 1;
+      margin-bottom: 30px;
+    }
+    .labels {
+      color: $primary;
+      span {
+        &::after {
+          content: "\00b7"; // Labels 가운데 점 표시
+          margin: 0 6px; 
+        }
+        &:last-child::after { // 마지막 점 제거
+          display: none;
+        }
+      }
+    }
+    .plot {
+      margin-top: 20px;
+    }
+    .ratings {
+
+    }
+    h3 {
+      margin: 24px 0 6px;
+      color: $black;
+      font-family: "Oswald", sans-serif;
+      font-size: 20px;
     }
   }
 }
